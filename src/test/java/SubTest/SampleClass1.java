16af87f5da33f1b29e0a0a4e7079e9269b4dca8e
Microsoft <Microsoft@LAPTOP-5NKJ9H0I>
2023-01-04 16:46:13 +0530

    i send it 2 classes please check and commit me back

---
+++ src/test/java/SubTest/SampleClass1.java
@@ -0,0 +1,8 @@
+package SubTest;
+
+public class SampleClass1 {
+	public static void main(String[] args) {
+		System.out.println("Good morning");
+	}
+
+}
---
+++ src/test/java/SubTest/SampleClass2.java
@@ -0,0 +1,10 @@
+package SubTest;
+
+import javax.management.openmbean.OpenMBeanAttributeInfo;
+
+public class SampleClass2 {
+	public static void main(String[] args) {
+		System.out.println("Good night");
+	}
+
+}
